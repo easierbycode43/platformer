e9c34250aa46ba5a1759517936be6192377ac0ab
EasierByCode[43] <daniel@community43.org>
2021-12-20 14:14:15 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,148 @@
+<script>
+  import Phaser from 'phaser'
+  import {
+    Game,
+    Scene,
+    Camera,
+    Tilemap,
+    TileLayer,
+    Spawner,
+  } from 'svelte-phaser'
+  import LoadingBar from './LoadingBar.svelte'
+  import Player from './Player.svelte'
+
+  let game
+
+  $: window.game = game
+
+  function preload(scene) {
+    scene.load.tilemapTiledJSON('tilemaps/castle', 'assets/tilemap.json')
+    scene.load.image('tilesets/castle', 'assets/castle-tileset.png')
+    scene.load.spritesheet('textures/mario', 'assets/mario.png', {
+      frameWidth: 21,
+      frameHeight: 27,
+    })
+    scene.load.spritesheet('textures/player', 'assets/adventurer.png', {
+      frameWidth: 50,
+      frameHeight: 37,
+    })
+  }
+
+  function create(scene) {
+    scene.anims.create({
+      key: 'anims/mario/idle',
+      frames: scene.anims.generateFrameNumbers('textures/mario', {
+        start: 2,
+        end: 2,
+      }),
+      frameRate: 6,
+      repeat: -1,
+    })
+    // scene.anims.create({
+    //   key: 'anims/player/idle',
+    //   frames: scene.anims.generateFrameNumbers('textures/player', {
+    //     start: 0,
+    //     end: 3,
+    //   }),
+    //   frameRate: 6,
+    //   repeat: -1,
+    // })
+    scene.anims.create({
+      key: 'anims/mario/run',
+      frames: scene.anims.generateFrameNumbers('textures/mario', {
+        start: 0,
+        end: 2,
+      }),
+      frameRate: 8,
+      repeat: -1,
+    })
+    // scene.anims.create({
+    //   key: 'anims/player/run',
+    //   frames: scene.anims.generateFrameNumbers('textures/player', {
+    //     start: 8,
+    //     end: 13,
+    //   }),
+    //   frameRate: 8,
+    //   repeat: -1,
+    // })
+    scene.anims.create({
+      key: 'anims/mario/jump',
+      frames: [
+        { key: 'textures/mario', frame: 0 },
+        { key: 'textures/mario', frame: 2 },
+        { key: 'textures/mario', frame: 3 },
+        { key: 'textures/mario', frame: 4 },
+        { key: 'textures/mario', frame: 5 },
+        { key: 'textures/mario', frame: 6 },
+        { key: 'textures/mario', frame: 2 },
+        { key: 'textures/mario', frame: 0 }
+      ],
+      frameRate: 24,
+    })
+    scene.anims.create({
+      key: 'anims/mario/fall',
+      frames: [
+        { key: 'textures/mario', frame: 0 }
+      ],
+      frameRate: 10,
+      repeat: -1,
+    })
+  }
+</script>
+
+<Game
+  bind:instance={game}
+  width={384}
+  height={216}
+  physics={{ default: 'arcade', arcade: { gravity: { y: 800 } } }}
+  render={{ pixelArt: true }}
+  scale={{ mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }}
+  backgroundColor='#80A8F8'
+>
+  <Scene
+    key="main"
+    {preload}
+    {create}
+    let:progress
+  >
+    <slot slot="loading">
+      <LoadingBar x={128} y={112} {progress} />
+    </slot>
+
+    <Spawner>
+      <Tilemap
+        key="tilemaps/castle"
+        tilesets={[{ key: 'tilesets/castle', name: 'castle-tileset' }]}
+      >
+        <TileLayer
+          id="ground"
+          name="ground"
+          tilesets={['castle-tileset']}
+          collisionByProperty={{ collision: true }}
+        />
+        <TileLayer id="bg" tilesets={['castle-tileset']} />
+
+      </Tilemap>
+    </Spawner>
+    <Camera
+      x={0}
+      y={0}
+      width={384}
+      height={216}
+      follow="mario"
+      roundPixels
+      bounds={{ x: 0, y: 0, width: (32 * 16), height: (16 * 16) }}
+    >
+      <Player x={124} y={124} />
+    </Camera>
+  </Scene>
+</Game>
+
+<style>
+  :global(body) {
+    margin: 0;
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/Player.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+  import Phaser from 'phaser'
+  import {
+    Sprite,
+    ArcadePhysics,
+    getScene,
+    onGameEvent,
+    ArcadeCollider,
+  } from 'svelte-phaser'
+
+  export let x: number
+  export let y: number
+
+  let flipX = false
+  let velocityX = 0
+  let velocityY = 0
+  let animation = 'idle'
+
+  let instance: Phaser.GameObjects.Sprite & { body: Phaser.Physics.Arcade.Body }
+
+  const scene = getScene()
+  const X_SPEED = 100
+  const keys = {
+    left: scene.input.keyboard.addKey('left'),
+    right: scene.input.keyboard.addKey('right'),
+    up: scene.input.keyboard.addKey('up'),
+    jump: scene.input.keyboard.addKey('SPACE'),
+  }
+
+  // handle key inputs
+  onGameEvent('step', () => {
+    if (
+      (!keys.left.isDown && !keys.right.isDown) ||
+      (keys.left.isDown && keys.right.isDown)
+    ) {
+      velocityX = 0
+    } else if (keys.left.isDown) {
+      velocityX = -X_SPEED
+      flipX = true
+    } else if (keys.right.isDown) {
+      flipX = false
+      velocityX = X_SPEED
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(keys.jump) && instance.body.onFloor()) {
+      velocityY = -200
+      animation = 'jump'
+    } else if (Phaser.Input.Keyboard.JustUp(keys.jump) && velocityY < 0) {
+      velocityY = 1
+    }
+  })
+
+  $: if (velocityY === 0) {
+    if (velocityX !== 0) {
+      animation = 'run'
+    } else {
+      animation = 'idle'
+    }
+  }
+
+  $: if (velocityY < 0 && animation !== 'fall') {
+    animation = 'jump'
+  } else if (velocityY > 0) {
+    animation = 'fall'
+  }
+
+  function onAnimationComplete({ detail }) {
+    if (detail.animation.key === 'anims/mario/jump') {
+      animation = 'fall'
+    }
+  }
+</script>
+
+<Sprite
+  name="mario"
+  bind:instance
+  bind:x
+  bind:y
+  animation={`anims/mario/${animation}`}
+  on:animationcomplete={onAnimationComplete}
+  {flipX}
+>
+  <ArcadePhysics
+    collideWorldBounds
+    bind:velocityX
+    bind:velocityY
+    width={21}
+    height={27}
+  />
+  <ArcadeCollider with="ground" />
+</Sprite>
